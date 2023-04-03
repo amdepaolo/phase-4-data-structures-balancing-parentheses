e641260b85fdaa7b756ca39842ef861df6e59502
Adam DePaolo <amdepaolo@gmail.com>
2023-04-02 21:23:21 -0400

    commit tree 2dc06f5a45d6e62435952d4030ef92aaa9c21f5a

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,15 @@
 require_relative './stack'
 
 # your code here
+
+def balancing_parentheses (string)
+    stack = Stack.new
+    string.chars.each do |char|
+        unless char == ')' && stack.peek == '('
+            stack.push char
+        else stack.pop
+        end
+    end
+
+    stack.size
+end
